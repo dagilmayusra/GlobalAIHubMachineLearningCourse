90cc07566f08daf916cc099a5b11116f0381cd00
Yüsra <76865219+devyusss@users.noreply.github.com>
2021-03-24 00:33:48 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,24 @@
-#Explain your work
+#Homework 1 - Yüsra DAĞILMA
 
-#Question 1
-for x in range(a):
-	print(a)
+#1) How would you define Machine Learning?
+
+	# It is a sub-branch of artificial intelligence.Machine learning is gaining skills equivalent to human intelligence without programming computers.
+
+#2) What are the differences between Supervised and Unsupervised Learning? Specify example 3 algorithms for each of these.
+
+	# Supervised learning --> Your class number is certain, you have trained data.It tries to make a new estimate by giving input and output values.
+		# Breast cancer research, benign or malignant tumors
+		# Real estate company research, finding the relationship between house size and house prices and determining the price for a new house to be sold
+		# Estimating the age of a person when given a picture
+	# Unsupervised learning --> Creating certain groups with the Clustering method according to the characteristics of the data you have.
+		# It is tried to discover whether people who buy product X tend to buy Y product as well.
+		# Marketers discover groups of customers based on interests to develop different marketing programs
+		#People can analyze large volumes of emails that they cannot analyze and reveal features and patterns that indicate spam and improve this feature over time.
+
+#3) What are the test and validation set, and why would you want to use them?
+
+#4) What are the main preprocessing steps? Explain them in detail. Why we need to prepare our data?
+
+#5) How you can explore countionus and discrete variables?
+
+#6) Analyse the plot given below. (What is the plot and variable type, check the distribution and make comment about how you can preproccess it.)
